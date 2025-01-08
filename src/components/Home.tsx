6cb0f2e689f8d9ea7bb01b4d4838326202ce1024
MAMCB <96302464+MAMCB@users.noreply.github.com>
2025-01-08 15:42:36 +0100

    imported fonts for italic and bold and set basic classes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-2xl m-5">Welcome to Telekom's project platform</h1>
+      <h1 className="text-2xl m-5 telekom-title">Welcome to Telekom's project platform</h1>
       <div className="flex justify-evenly bg-gray-300 rounded-lg
       p-4">
         <h2>News</h2>
