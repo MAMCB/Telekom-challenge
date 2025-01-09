21be2d5b436e827f7dc80538333fdb01203d508b
MAMCB <96302464+MAMCB@users.noreply.github.com>
2025-01-09 14:17:01 +0100

    created main components and set main routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+import Projects from "./components/Projects";
+import Events from "./components/Events";
 import "./App.css";
 
 function App() {
@@ -8,10 +10,14 @@ function App() {
   <Router>
     <nav className="flex justify-evenly">
       <Link to='/'>Home</Link>
+      <Link to='/events'>Events</Link>
+      <Link to='/projects'>Projects</Link>
       <Link to='/profile'>Profile</Link>
     </nav>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/projects" element={<Projects />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
     </Router>
